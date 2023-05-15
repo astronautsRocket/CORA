db8079dcf27d50714df81d4d76e0446e5e2cc2b7
Kartikey Pandey <121862945+astronautsRocket@users.noreply.github.com>
2023-05-16 04:15:04 +0530

    assiments

---
+++ asssi.c
@@ -0,0 +1,257 @@
+        //wap which display your name 15 times//
+// #include<stdio.h>
+// int main()
+// {
+    // int a=1,b=15;
+    // while (a<=b)
+    // {
+        // printf("Kartikey \t");
+        // a++;
+    // }
+    
+// }
+            // wap which display value from 1 to 20//
+// #include<stdio.h>
+// int main()
+// {
+//     int a=1,b=20;
+//     while (a<=b)
+//     {
+//         printf("%d \t",a);
+//         a++;
+//     }
+    
+// }
+              /* WAP which takes ending number from user and display values from 1 to that number. */
+// #include<stdio.h>
+// int main()
+// {
+//     int a=1,b;
+//     printf("enter the number");
+//     scanf("%d",&b);
+//     while (a<=b)
+//     {
+//         printf("%d \t",a);
+//         a++;
+//     }
+    
+// }
+/* wap which display 5 to 1 */
+/* #include<stdio.h>
+int main()
+{
+    int a=5,b=1;
+    while (a>=b)
+    {
+       printf("%d \t",a);
+       a--;         
+    }
+    
+} */
+/* WAP which display value 1 to 30 except 15 */
+/* #include<stdio.h>
+int main()
+{
+    int a=1,b=30;
+    while (a<=b)
+    {
+        printf("%d \t",a);
+        a++;
+        if(a==15)
+        {
+            a++;
+            continue;
+        }
+    }
+} */
+/* WAP that display all even number from 1 to 50 */
+/* #include<stdio.h>
+int main()
+{
+    int a=1,b=50;
+    while (a<=b)
+    {
+        if (a%2==0)
+        {
+            printf("%d \t",a);
+        }
+        a++;
+    } 
+    
+}*/
+/* WAP which takes a range (Starting and Ending number) from user and display all values.  */
+/* #include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter starting number");
+    scanf("%d",&a);
+    printf("enter ending number");
+    scanf("%d",&b);
+    while (a<=b)
+    {
+       printf("%d \t",a);
+       a++;1
+    }
+} */
+/* WAP that take range (Starting and Ending number) from user and display all odd number in 
+between them. */
+/* #include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter starting number");
+    scanf("%d",&a);
+    printf("enter ending number");
+    scanf("%d",&b);
+    while (a<=b)
+    {
+       if (a%2==0)
+       {
+         printf("%d",a);
+       }
+       a++;
+    }  
+}  */
+
+
+        //wap which display your name 15 times//
+// #include<stdio.h>
+// int main()
+// {
+    // int a=1,b=15;
+    // while (a<=b)
+    // {
+        // printf("Kartikey \t");
+        // a++;
+    // }
+    
+// }
+            // wap which display value from 1 to 20//
+// #include<stdio.h>
+// int main()
+// {
+//     int a=1,b=20;
+//     while (a<=b)
+//     {
+//         printf("%d \t",a);
+//         a++;
+//     }
+    
+// }
+              /* WAP which takes ending number from user and display values from 1 to that number. */
+// #include<stdio.h>
+// int main()
+// {
+//     int a=1,b;
+//     printf("enter the number");
+//     scanf("%d",&b);
+//     while (a<=b)
+//     {
+//         printf("%d \t",a);
+//         a++;
+//     }
+    
+// }
+/* wap which display 5 to 1 */
+/* #include<stdio.h>
+int main()
+{
+    int a=5,b=1;
+    while (a>=b)
+    {
+       printf("%d \t",a);
+       a--;         
+    }
+    
+} */
+/* WAP which display value 1 to 30 except 15 */
+/* #include<stdio.h>
+int main()
+{
+    int a=1,b=30;
+    while (a<=b)
+    {
+        printf("%d \t",a);
+        a++;
+        if(a==15)
+        {
+            a++;
+            continue;
+        }
+    }
+} */
+/* WAP that display all even number from 1 to 50 */
+/* #include<stdio.h>
+int main()
+{
+    int a=1,b=50;
+    while (a<=b)
+    {
+        if (a%2==0)
+        {
+            printf("%d \t",a);
+        }
+        a++;
+    } 
+    
+}*/
+/* WAP which takes a range (Starting and Ending number) from user and display all values.  */
+/* #include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter starting number");
+    scanf("%d",&a);
+    printf("enter ending number");
+    scanf("%d",&b);
+    while (a<=b)
+    {
+       printf("%d \t",a);
+       a++;1
+    }
+} */
+/* WAP that take range (Starting and Ending number) from user and display all odd number in 
+between them. */
+/* #include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter starting number");
+    scanf("%d",&a);
+    printf("enter ending number");
+    scanf("%d",&b);
+    while (a<=b)
+    {
+       if (a%2==0)
+       {
+         printf("%d",a);
+       }
+       a++;
+    }  
+}  */
+/* WAP which takes a number from user and display table of that number */
+/* #include<stdio.h>
+int main()
+{
+    int a=1,b,c;
+    printf("enter a number ");
+    scanf("%d",&b);
+    while (a<=10)
+    {
+        c=b*a;
+        printf("%d \n",c);
+        a++;
+    }
+} */
+/* . WAP Which takes a number from user and display table in formatted way */
+#include<stdio.h>
+int main()
+{
+    int a=1,b=7;
+    while (a<=10)
+    {
+        printf("%d 7*a=",a*b);
+        a++;
+    }
+}
